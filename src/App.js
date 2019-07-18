d18eca4b5936a5fde5b229a0aad0bea77defc8ff
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-07-18 15:13:46 -0500

    fixed jest version to run client, created main display structure, basic initial styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import './App.css';
 
-class AddPlant extends Component {
-  render {
-  return (
-    <div>
-      <header>
-        <h1>My Retail RESTful Service App</h1>
-      </header>
-    </div>
-  );
-}
+class Main extends Component {
+  state = {
+    productId: '',
+  }
+
+  handleChange = ( event ) => {
+    this.setState({
+      productId: event.target.value
+    })
+  }
+
+  handleClick = ( event ) => {
+    console.log( `productId is`, this.state.productId );
+    event.preventDefault();
+    // this.getProductDetails();
+  }
+
+  render () {
+    return (
+      <div class="main-content">
+        { JSON.stringify( this.state ) }
+        <header>
+          <h1>My Retail RESTful Service App</h1>
+        </header>
+        <form>
+          <label for="product-id">Please enter a product id:</label>
+          <input onChange={ this.handleChange } placeholder="i.e. 44357291" value={ this.state.productId } id="product-id"/>
+          <button onClick={ this.handleClick }>Get Details</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Main;
