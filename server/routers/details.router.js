07a9b9e314d1a02e25839aa161e6a91d19eb27b6
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-07-18 15:53:27 -0500

    details.router.js: created initial mongoose schema and model

--- server/routers/details.router.js
+++ server/routers/details.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool.js');
 const mongoose = require('mongoose');
 
 // connect to mongo using mongoose
-const databaseUrl = 'mongodb://localhost:27017/customRedskyPricing'
+const databaseUrl = 'mongodb://localhost:27017/customRedskyPricing';
 congoose.connect(databseUrl, { useNewUrlParser: true });
 
 // log when connected
@@ -16,3 +16,14 @@ mongoose.connection.once('connected', () => {
 mongoose.connection.on('error', () => {
     console.log(`error connecting mongoose to db: `, error);
 });
+
+// creating a mongoose schema
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+    productId: { type: Number, require: true },
+    productName: { type: String, require: true },
+    price: { type: Number, require: false }
+});
+
+// creating a mongoose model
+const Product = mongoose.model('Product', productSchema);
